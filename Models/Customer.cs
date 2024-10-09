2ff9ebbe91162d3aa2b6a02a8e6239d91d071b25
jasbir purewal <jasbir_purewal@hotmail.co.uk>
2024-10-09 15:29:06 +0100

    Update validation

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,14 +1,29 @@
-﻿namespace CustomersWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomersWebApp.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)] 
         public string Name { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)]
         public string Address { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string City { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string PostCode { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
         public string Country { get; set; }
+
+        [StringLength(20,MinimumLength = 10)]
         public string Phone { get; set; }
+        
         public Customer()
         {
             Id= 0;
